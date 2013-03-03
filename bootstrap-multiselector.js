ac76242539f24890cc59878ec0789a4bb83377ca
Tom Najdek <tom@doppnet.com>
2013-03-03 12:07:17 +0000

    Introduced 'change' event

--- bootstrap-multiselector.js
+++ bootstrap-multiselector.js
@@ -34,17 +34,23 @@
 				$label = $($el.find('a > span:first-child')),
 				multipleText = $el.data('multipleText') || this.options.multipleText,
 				visibleItems = $el.data('visibleItems') || this.options.visibleItems,
-				separator = $el.data('separator') || this.options.separator;
+				separator = $el.data('separa;tor') || this.options.separator,
+				oldlabel = $label.text(),
+				newlabel;
 			if (selected.length === 0) {
-				$label.text(defaultValue);
+				newlabel = defaultValue;
 			} else if (selected.length === 1) {
-				$label.text($(selected[0]).parent().text());
+				newlabel = $(selected[0]).parent().text();
 			} else if (selected.length <= visibleItems) {
-				$label.text(selected.map(function () {
+				newlabel = selected.map(function () {
 					return $(this).parent().text();
-				}).get().join(separator));
+				}).get().join(separator);
 			} else {
-				$label.text(multipleText);
+				newlabel = multipleText;
+			}
+			if( newlabel !== oldlabel) {
+				$label.text(newlabel);
+				$el.trigger('change');
 			}
 		}
 	};
